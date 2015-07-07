04b2aa775bad25af8d550078308acf201ebc7dae
zyc <zyc@hasor.net>
2015-07-07 20:47:35 +0800

    1.fix几处javadoc错误。

--- hasor-core/src/main/java/net/hasor/db/jdbc/core/JdbcTemplate.java
+++ hasor-core/src/main/java/net/hasor/db/jdbc/core/JdbcTemplate.java
@@ -87,9 +87,9 @@ public class JdbcTemplate extends JdbcConnection implements JdbcOperations {
     }
 
     /**
-     * Construct a new JdbcTemplate, given a DataSource to obtain connections from.
+     * Construct a new JdbcTemplate, given a Connection to obtain connections from.
      * <p>Note: This will not trigger initialization of the exception translator.
-     * @param dataSource the JDBC DataSource to obtain connections from
+     * @param conn the JDBC Connection
      */
     public JdbcTemplate(final Connection conn) {
         super(conn);
