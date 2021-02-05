6580a14236f7b3e801654997da42e1e5d7ceb25f
zyc <zyc@hasor.net>
2021-02-05 15:52:19 +0800

    1. 文件头部 License 内容是否缺失。

--- hasor-db/src/test/java/net/hasor/test/db/dialect/TestDialect.java
+++ hasor-db/src/test/java/net/hasor/test/db/dialect/TestDialect.java
@@ -13,43 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.hasor.db.dialect;
-import net.hasor.utils.StringUtils;
+package net.hasor.test.db.dialect;
+import net.hasor.core.exts.aop.Aop;
+import net.hasor.db.dialect.BoundSql;
+import net.hasor.db.dialect.SqlDialect;
 
 import java.sql.JDBCType;
 
-/**
- * 默认 SqlDialect 实现
- * @version : 2020-10-31
- * @author 赵永春 (zyc@hasor.net)
- */
-public class DefaultSqlDialect implements SqlDialect {
+@Aop(TestDialectAop.class)
+public class TestDialect implements SqlDialect {
     @Override
     public String buildSelect(String category, String tableName, String columnName, JDBCType jdbcType, Class<?> javaType) {
-        return columnName;
+        return null;
     }
 
     @Override
     public String buildTableName(String category, String tableName) {
-        if (StringUtils.isBlank(category)) {
-            return tableName;
-        } else {
-            return category + "." + tableName;
-        }
+        return null;
     }
 
     @Override
     public String buildColumnName(String category, String tableName, String columnName, JDBCType jdbcType, Class<?> javaType) {
-        return columnName;
-    }
-
-    @Override
-    public BoundSql getCountSql(BoundSql boundSql) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public BoundSql getPageSql(BoundSql boundSql, int start, int limit) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 }
